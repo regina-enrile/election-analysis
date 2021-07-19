d12cdd245219db4bbe5d1355f3197fd4c112f0e9
Regina Enrile <regina_enrile@hotmail.com>
2021-07-19 03:34:37 -0400

    3.3.1

--- python_practice.py
+++ python_practice.py
@@ -14,7 +14,72 @@ if "Arapahoe" in counties or "El Paso" in counties:
 else: print("Arapahoe and El Paso are not in the list of counties.")
 
 if "Arapahoe" in counties and "El Paso" not in counties:
-       print("Only Arapahoe is in the list of counties.")
+    print("Only Arapahoe is in the list of counties.")
 else:
     print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
-    
+
+#################
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+
+for county in counties_dict.keys():
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+for county, voters in counties_dict.items():
+    print(county, voters)
+
+##########################
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+            {"county":"Denver", "registered_voters": 463353},
+            {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+####################### 3_2_11
+
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+
+my_votes = input("How many votes did you get in the election? ")
+total_votes = input("What is the total votes in the election? ")
+percentage_votes = my_votes / total_votes * 100
+print("I received " + (percentage_votes)+"% of the total votes.")
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + (voters) + " registered voters.")
+
+
+
+
+candidate_votes = input("How many votes did the candidate get in the election? ")
+total_votes = input("What is the total number of votes in the election? ")
+message_to_candidate = (
+    "You received {candidate_votes} number of votes. "
+    "The total number of votes in the election was {total_votes}. "
+    "You received {candidate_votes // total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
+
+##########################
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+
